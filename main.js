c55a3ccc9db5146440d8f9014e450c36ad75a797
krolewnaoskar <oskar.gplay225@gmail.com>
2020-04-28 21:21:08 +0200

    my first repository

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("pierwszy skrypt oskara");
+var button=document.querySelector("#nazwa");
+console.log(button);
+function pokazAlert(){
+    alert("jestem debilem");
+
+}
+
+document.querySelector("#nazwa").addEventListener("click",pokazAlert);
